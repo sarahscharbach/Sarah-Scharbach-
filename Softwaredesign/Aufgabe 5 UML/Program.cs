8c7956e96c0f925939cebc8b992c2642c39ca89f
Sarah SCharbach <sarah.scharbach@hs-furtwangen.de>
2018-05-28 14:11:44 +0200

    Aufgabe 5 UML

---
+++ Softwaredesign/Aufgabe 5 UML/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aufgabe_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teilnehmer teilnehmer_01 = new Teilnehmer();
+            teilnehmer_01.vorname = "Franz";
+            teilnehmer_01.nachname = "Schulz";
+            teilnehmer_01.matrikelnummer = 12345;
+            teilnehmer_01.alter = 22;
+
+            Kurs kurs_01 = new Kurs();
+            kurs_01.titel = "Programmieren";
+            kurs_01.vorlesungssaal = "C 1.10";
+            kurs_01.wochentag = "Donnerstag";
+            kurs_01.uhrzeit = "11.15";
+            kurs_01.InfoText();
+        }
+    }
+}
